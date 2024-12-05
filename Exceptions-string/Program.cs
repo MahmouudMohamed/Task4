fb9d0db84e20b48b810644ec7fa4f84273d5d0d5
Mahmoud Ouf <mahmoudouf504@gmail.com>
2024-12-05 20:11:40 +0200

    Exception string

---
+++ Exceptions-string/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exceptions_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStatement;
+            Console.Write("Enter Your Statment : ");
+            inputStatement = Console.ReadLine().ToUpper();
+            try
+            {
+                if (!inputStatement.Contains('A') && !inputStatement.Contains('E') && !inputStatement.Contains('I') &&
+                    !inputStatement.Contains('O') && !inputStatement.Contains('U'))
+                {
+                    throw new Exception("Your Statment Doesn't Conatin Vowels");
+                }
+                else
+                {
+                    Console.WriteLine($"The Statment is {inputStatement}");
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
